cd4e2ecb1c3fea88368992010f8a8cc9029ae7be
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-11-21 20:48:19 +0200

    Internationalisation

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +15,29 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+//	@Bean
+//	public LocaleResolver localeResolver() {
+//		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//		localeResolver.setDefaultLocale(Locale.US);
+//		return localeResolver;
+//	}
+
+	// Gets the locale from the Accept-Language header in the request
+	@Bean
+	public LocaleResolver acceptHeaderLocaleResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	// This can be set in application.properties, and then this method isn't
+	// required
+//	@Bean
+//	public ResourceBundleMessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//
+//		messageSource.setBasename("messages");
+//
+//		return messageSource;
+//	}
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	// @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
 	// @GetMapping(path = "/hello-world")
@@ -22,5 +28,20 @@ public class HelloWorldController {
 	@GetMapping({ "/hello-world/{name}" })
 	public HelloWorldBean helloWorld(@PathVariable String name) {
 		return new HelloWorldBean("Hello World, " + name);
+	}
+
+	// This reads in the language from the Accept-Language header of the request
+//	@GetMapping({ "/hello-world-i18n/{name}" })
+//	public HelloWorldBean helloWorldInternationalised(
+//			@RequestHeader(name = "Accept-Language", required = false) Locale locale, @PathVariable String name) {
+//		return new HelloWorldBean(messageSource.getMessage("hello-world", null, locale) + ", " + name);
+//	}
+
+	// This uses the AcceptHeaderLocaleResolver, which means we don't need to read
+	// it in, in every method
+	@GetMapping({ "/hello-world-i18n-auto/{name}" })
+	public HelloWorldBean helloWorldInternationalisedAuto(@PathVariable String name) {
+		return new HelloWorldBean(
+				messageSource.getMessage("hello-world", null, LocaleContextHolder.getLocale()) + ", " + name);
 	}
 }
